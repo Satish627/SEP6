0e35a53addb3b60f5ce805b15befa394d353fd66
satish627 <satish.grg627@gmail.com>
2022-12-03 19:51:58 +0100

    Modal added

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -11,32 +11,39 @@ const MovieInfo = ({ title, poster_path, vote_average, release_date, overview })
     const handleClose = () => setShow(false);
     return (
         <div className="card text-center m-2 ">
-            <div className="card-body">
-                <img className="card-img-top" src={IMG_API + poster_path} alt="some images" />
-                <div className="card-body">
-                    <h5>Rating: {vote_average}</h5>
+            <div></div>
+            <img className="card-img-top " src={IMG_API + poster_path} alt="some images" />
 
-                    <Button type="button" className="btn btn-warning" onClick={handleShow}>
-                        Show more
-                    </Button>
-                    <Modal modal-dialog modal-sm show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title></Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <img className="card-img-top" style={{ height: 600 }} src={IMG_API + poster_path} alt="some images"></img>
-                            <h3 className="mt-2">{title}</h3>
-                            <h4>Release Date: {release_date}</h4>
-                            <h5>Rating : {vote_average}</h5>
-                            <br></br>
-                            <h6>Description </h6>
-                            <p>{overview}</p>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button className="btn btn-warning" onClick={handleClose}>Close</Button>
-                        </Modal.Footer>
-                    </Modal>
+            <div className="card-body " >
+                <div className="d-flex justify-content-between">
+                    <h4 className="mr-auto">{title}</h4>
+                    <h5  >{vote_average}</h5>
+
+
                 </div>
+                    
+                <Button type="button" className="btn btn-warning" onClick={handleShow}>
+                    Show more
+                </Button>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title></Modal.Title>
+                    </Modal.Header>
+                    <img className="card-img-top" style={{ height: 600 }} src={IMG_API + poster_path} alt="some images"></img>
+
+                    <Modal.Body>
+                        <h3 className="mt-2">{title}</h3>
+                        <h4>Release Date: {release_date}</h4>
+                        <h5>Rating : {vote_average}</h5>
+                        <br></br>
+                        <h6>Description </h6>
+                        <p>{overview}</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button className="btn btn-warning" onClick={handleClose}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+
             </div>
 
         </div>
