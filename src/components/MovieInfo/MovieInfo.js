f5d7b7d7609b14eebaca5823ef27068e77476839
satish627 <satish.grg627@gmail.com>
2022-12-14 23:15:34 +0100

    responsiveness added

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,7 +1,6 @@
 import { Modal, Button } from "react-bootstrap";
 import React, { useState } from "react";
 import star from "../../assets/star.webp"
-import "./MovieInfo.css"
 
 const IMG_API = "https://image.tmdb.org/t/p/w500";
 
@@ -16,21 +15,25 @@ const MovieInfo = ({ title, poster_path, vote_average, release_date, overview })
             <img className="card-img-top " src={IMG_API + poster_path} alt="some images" />
 
             <div className="card-body " >
-                <div className="d-flex justify-content-between">
-                    <h5 className="me-5 ">{title}</h5>
-                    <div className="d-flex">
-                        <img src={star} alt="rating" className="star"></img>
-                        <h5 className="ms-2">{vote_average}</h5>
+                <div className="d-flex-col justify-content-between">
+                    <div className="d-flex justify-content-around">
+                        <h5>{title}</h5>
+                        <div className="d-flex">
+                            <img src={star} alt="rating" style={{ width: 20, height: 20 }}></img>
+                            <h5 className="ms-2">{vote_average}</h5>
 
+                        </div>
                     </div>
-                </div>
 
-                <Button type="button" className="btn btn-warning mt-2 me-2" onClick={handleShow}>
-                    Show more
-                </Button>
-                <Button type="button" className="btn btn-warning mt-2" >
-                    Add to favorites 
-                </Button>
+
+
+                    <Button type="button" className="btn btn-warning mt-2 me-2" onClick={handleShow}>
+                        Show more
+                    </Button>
+                    <Button type="button" className="btn btn-warning mt-2" >
+                        Add to favorites
+                    </Button>
+                </div>
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
                         <Modal.Title></Modal.Title>
@@ -40,8 +43,8 @@ const MovieInfo = ({ title, poster_path, vote_average, release_date, overview })
                     <Modal.Body>
                         <h3 className="mt-2">{title}</h3>
                         <h4>Release Date: {release_date}</h4>
-                        <div className="d-flex">
-                            <img src={star} alt="rating" className="star"></img>
+                        <div className="d-flex justify-content">
+                            <img src={star} alt="rating" style={{width:20, height:20}}></img>
                             <h5 className="ms-2">{vote_average}</h5>
 
                         </div>
